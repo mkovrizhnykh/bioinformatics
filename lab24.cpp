741830dd76f042d089e41c5ba1131a877369b088
Мария Коврижных <43151557+mkovrizhnykh@users.noreply.github.com>
2021-04-03 17:05:10 +0300

    Update lab24.cpp

--- lab24.cpp
+++ lab24.cpp
@@ -1,4 +1,4 @@
-#include<vector>
+#include <vector>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -8,29 +8,17 @@
 using namespace std;
 
 int main() {
-	std::ifstream in("C:/Users/Пользователь/Desktop/masstable.txt");
-	vector <char> let,inp;
-	vector<int> num,sum,final;
-	string line;
-	char iinp, llet;
+	vector <char> let, inp;
+	vector<int> num, sum, final;
+	string line, iinp;
+	char llet;
 	int nnum, ssum = 0;
-	for (int i = 3; i >= 0; i--) {
-		cin >> iinp;
-		inp.push_back(iinp);
-	}
-
-	if (in.is_open())
-	{
-		while (getline(in, line)) {
-			istringstream iss(line);
-			iss >> llet >> nnum;
-			let.push_back(llet);
-			num.push_back(nnum);
-		} in.close();
-	} else {
-
-		cout << "We Havet file";
-	}
+	cin >> iinp;
+	for(int i=0;i<iinp.size();i++)
+	inp.push_back(iinp[i]);
+	num = {57,71,87,97,99,101,103,113,113,114,115,128,128,129,131,137,147,156,163,186};
+	let = { 'G','A','S','P','V','T','C','I','L','N','D','K','Q','E','M','H','F','R','Y','W'};
+	
 	sort(sum.begin(), sum.end());
 	for (int i = 0; i < let.size(); i++) {
 		if ((let[i] == inp[0]) || (let[i] == inp[1]) || (let[i] == inp[2]) || (let[i] == inp[3])) sum.push_back(num[i]);
@@ -38,10 +26,10 @@ int main() {
 
 	for (int i = 4; i >= 0; i--) {
 		if (i == 4) {
-			cout << ssum <<" ";
+			cout << ssum << " ";
 		} if (i == 3) {
-			for (int j = 0; j < sum.size();j++)
-			cout << sum[j] << " ";
+			for (int j = 0; j < sum.size(); j++)
+				cout << sum[j] << " ";
 		} if (i == 2) {
 			final.push_back(sum[0] + sum[1]);
 			final.push_back(sum[1] + sum[2]);
@@ -49,7 +37,7 @@ int main() {
 			final.push_back(sum[0] + sum[3]);
 			sort(final.begin(), final.end());
 			for (int j = 0; j < final.size(); j++)
-					cout << final[j] << " ";
+				cout << final[j] << " ";
 			final.clear();
 		} if (i == 1) {
 			final.push_back(sum[0] + sum[1] + sum[2]);
@@ -64,6 +52,6 @@ int main() {
 			cout << sum[0] + sum[1] + sum[2] + sum[3] << " ";
 		}
 	}
-	system("pause");
+	//system("pause");
 	return 0;
 }
